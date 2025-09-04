29fd46385bfba3250311ba7feff8768db91fc23b
Ahmed Elsaid <ahmed.muhammed.elsaid@gmail.com>
2025-09-04 15:45:16 +0300

    feat: add recurcsuin closure challenge

---
+++ recurcsionAndClosure/recurcsionAndClosure.js
@@ -0,0 +1,8 @@
+
+// 🤯 JavaScript Interview Challenge: Can you write this polyfill ?
+// console.log(sum(1)(2)(3)());  // ?
+// console.log(sum(1, 2)(3, 4)());  // ?
+// console.log(sum(5)());     // ?
+
+
+
