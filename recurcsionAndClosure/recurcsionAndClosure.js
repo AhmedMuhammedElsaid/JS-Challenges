de0df3e5b7c091aa8dc4d9b7fc661709d9aa4801
Ahmed Elsaid <ahmed.muhammed.elsaid@gmail.com>
2025-09-04 15:52:22 +0300

    feat: add the challenge implmentation

--- recurcsionAndClosure/recurcsionAndClosure.js
+++ recurcsionAndClosure/recurcsionAndClosure.js
@@ -6,3 +6,16 @@
 
 
 
+
+const sum = (...args) => {
+    let total = args.reduce((acc, curr) => acc + curr, 0)
+    const inner = (...nextArgs) => {
+        if (nextArgs.length === 0) return total;
+        total += nextArgs.reduce((acc, curr) => acc + curr, 0)
+        return inner
+    }
+    return inner
+}
+console.log(sum(1)(2)(3)());
+console.log(sum(1, 2)(3, 4)());  
+console.log(sum(5)());    
