3066b20f662c3e5b870a97aa1e08f8e30a3f7552
Ahmed Elsaid <ahmed.muhammed.elsaid@gmail.com>
2025-09-13 17:53:53 +0300

    feat: add flattenDeepIter implementaion using stack

--- Intermediate/FalttenArray/FlattenArray.js
+++ Intermediate/FalttenArray/FlattenArray.js
@@ -2,7 +2,7 @@
 // 👉 Input: [1,2,3,[4,5,6,[7,8,[10,11]]],9]
 // 👉 Output: [1,2,3,4,5,6,7,8,10,11,9]
 
-
+// Recursive 1
 function flatten(arr) {
     if (!Array.isArray(arr))  return [arr]
     const flatenArr = [];
@@ -10,4 +10,27 @@ function flatten(arr) {
     return flatenArr;
 }
 
-console.log(flatten([1,2,3,[4,[[[36,[[[[[[5]]],11],23],69],11,0],4]],5,6,[7,8,[10,11]]],9]))
+console.log(flatten([1, 2, 3, [4, [[[36, [[[[[[5]]], 11], 23], 69], 11, 0], 4]], 5, 6, [7, 8, [10, 11]]], 9]))
+
+// Recursive 2
+function flattenDeep(arr) {
+  const out = [];
+  (function dfs(a) {
+    for (const x of a) Array.isArray(x) ? dfs(x) : out.push(x);
+  })(arr);
+  return out;
+}
+
+//Iterative (stack, safer for huge depth):
+
+function flattenDeepIter(arr) {
+  const out = [];
+  const stack = [...arr].reverse();
+  while (stack.length) {
+    const v = stack.pop();
+    if (Array.isArray(v)) stack.push(...v.reverse());
+    else out.push(v);
+  }
+  return out;
+}
+
